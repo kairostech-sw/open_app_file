464dc62da34676ea558e51edef19297557da3f95
Yendacoder <it@yendoplan.com>
2023-06-29 10:58:59 +0200

    updated documentation

--- lib/src/common/open_result.dart
+++ lib/src/common/open_result.dart
@@ -1,9 +1,14 @@
+
+/// Result of open action
 class OpenResult {
+  /// Result type
   ResultType type;
+  /// Human readable message, adding some context to the possible error result
   String message;
 
   OpenResult(this.type, {this.message = "done"});
 
+  /// Construct result from the platform channel response
   OpenResult.fromJson(Map<String, dynamic> json)
       : type = _typeFromCode(json['type']),
         message = json['message'];
@@ -23,10 +28,16 @@ class OpenResult {
   }
 }
 
+/// Result type
 enum ResultType {
+  /// File opened successfully
   done,
+  /// File not found
   fileNotFound,
+  /// File exists, but system doesn't have an application to handle it
   noAppToOpen,
+  /// System denied permission to open the file (Android only)
   permissionDenied,
+  /// Generic error, check the message for details
   error,
 }
